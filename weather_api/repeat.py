a4f0a1ac3b04912d5d443eea57da332d3e040802
myungwoo-Y <auddn6676@naver.com>
2020-01-30 15:29:39 -0500

    add thread that  fetch weather datas

--- weather_api/repeat.py
+++ weather_api/repeat.py
@@ -1,11 +1,14 @@
-import time
+import time 
 from threading import Thread
 import requests
+from pymongo import MongoClient
+from pprint import pprint
+from weather_api.api_id import api_id
 
-class worker(Thread):
+class sensor_worker(Thread):
     def run(self):
         while(True):
-            lora_url = 'https://api.thingspeak.com/channels/956974/feeds.json?api_key=ZRTL71SHNNFL0KJ0&results=8000'
+            lora_url = 'https://api.thingspeak.com/channels/970723/feeds.json?api_key=AU0TNWBNLRYXU1QL&results=5'
             lora_request = requests.get(lora_url).json()
             datas = lora_request["feeds"]
             data = datas[len(datas) - 1]
@@ -14,33 +17,49 @@ class worker(Thread):
             field2 =  data["field2"]
             field3 =  data["field3"]
             
-            soil_moisture = int(field3) / 8
+            soil_moisture = float(field3)
 
-            print(soil_moisture)
+            print("soil_moisture : ", soil_moisture)
 
-            cookies = {'sysauth': 'c03412f3ac67293cb346e0896b52acc9'}
+            cookies = {'sysauth': '03dd9e823f79d05ff6129019918af23e'}
 
-            mad = 50
+            mad = 30
             if(soil_moisture < mad):
-                depth = 1
-                awc = 0.11
+                depth = 1.0
+                awc = 0.21
                 net_irr = awc * mad
-                efficiency_of_drip = 80
+                efficiency_of_drip = 80.0
+                
                 ga = net_irr / efficiency_of_drip
 
-                area = 16
-                flow_rate = 1.6
-                time = (ga * area) / (1.6 * flow_rate)
+                area = 0.11
+                flow_rate = 0.8
 
-                trigger_request = requests.get('http://192.168.43.69/arduino/1', cookies=cookies)
-                print(trigger_request, "request ON")
+                # time을 분으로 환산
+                irr_time = ((ga * area) / (1.6 * flow_rate)) * 3600
+
+                str_time = str(irr_time)
+
+                print("set time : ", irr_time)
+
+                # trigger_request = requests.get(' http://192.168.2.241/arduino/irrigation/' + str_time, cookies=cookies)
+                # print(trigger_request, "request ON")
             
             else:
-                trigger_request = requests.get('http://192.168.43.69/arduino/0', cookies=cookies)    
+                trigger_request = requests.get('http://192.168.2.241/arduino/irrigation/0', cookies=cookies)
                 print(trigger_request, "request Off")
-            
-            time.sleep(10)
+            time.sleep(5)
+
+class weather_worker(Thread):
+    def run(self):
+        while(True):
+            weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={},us&appid={}'
+            city = 'Lafayette '
+            weatehr_request = requests.get(weather_url.format(city, api_id)).json()
+            print(weatehr_request)
+            time.sleep(5)
 
 
 def one_time_startup():
-    worker().start()
+    # sensor_worker().start()
+    weather_worker().start()
