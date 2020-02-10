e4cf58960bc0442975a72d16718395b5101e1242
myungwoo-Y <auddn6676@naver.com>
2020-02-10 15:29:33 -0500

    add interval

--- weather_api/repeat.py
+++ weather_api/repeat.py
@@ -19,29 +19,27 @@ class sensor_worker(Thread):
         current_min = 1
 
         print("hour : " + times[current_hour])
+        print("min : " + times[current_min])
 
-        if(int(times[current_hour]) == 10):
+        if(int(times[current_hour]) == 9 and int(times[current_min]) < 30):
             return True
         else:
             return False
     
     def irrigation(self):
+        cookies = {'sysauth': '61fe7f9775453cc3b895be0da1202af8'}
+        lora_url = 'https://api.thingspeak.com/channels/970723/feeds.json?api_key=AU0TNWBNLRYXU1QL&results=5'
+        lora_request = requests.get(lora_url).json()
+        datas = lora_request["feeds"]
+        data = datas[len(datas) - 1]
+            
+        field1 =  data["field1"]
+        field2 =  data["field2"]
+        field3 =  data["field3"]
+       
+        soil_moisture = float(field3)
         if(self.check_hour()):
-            lora_url = 'https://api.thingspeak.com/channels/970723/feeds.json?api_key=AU0TNWBNLRYXU1QL&results=5'
-            lora_request = requests.get(lora_url).json()
-            datas = lora_request["feeds"]
-            data = datas[len(datas) - 1]
-            
-            field1 =  data["field1"]
-            field2 =  data["field2"]
-            field3 =  data["field3"]
-            
-            soil_moisture = float(field3)
-
             print("soil_moisture : ", soil_moisture)
-
-            cookies = {'sysauth': '7374d0a26214e7077278caef14ad7196'}
-
             mad = 30
             if(soil_moisture < mad):
                 depth = 1.0
@@ -58,17 +56,20 @@ class sensor_worker(Thread):
                 irr_time = int(((ga * area) / (1.6 * flow_rate)) * 3600)
 
                 str_irr_time = str(irr_time)
-
+                
                 print("set time : ", irr_time)
 
                 trigger_request = requests.get('http://192.168.2.241/arduino/irrigation/' + str_irr_time, cookies=cookies)
                 print('http://192.168.2.241/arduino/irrigation/' + str_irr_time)
                 print(trigger_request, "request ON")
-
+        else:
+            trigger_request = requests.get('http://192.168.2.241/arduino/irrigation/0', cookies=cookies)
+            print("soil_moisture : ", soil_moisture)
+            print(trigger_request, "request OFF")
     def run(self):
         while(True):
             self.irrigation()
-            time.sleep(3600)
+            time.sleep(20)
 
 class weather_worker(Thread):
     def run(self):
