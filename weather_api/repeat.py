6f1dc76c60d4113cf80ed83195b273809bdfd6e6
myungwoo-Y <auddn6676@naver.com>
2020-02-13 14:28:12 -0500

    push data when irrigation is occurred

--- weather_api/repeat.py
+++ weather_api/repeat.py
@@ -20,6 +20,8 @@ class sensor_worker(Thread):
 
         print("hour : " + times[current_hour])
         print("min : " + times[current_min])
+        
+        return True
 
         if(int(times[current_hour]) == 9 and int(times[current_min]) < 30):
             return True
@@ -27,7 +29,7 @@ class sensor_worker(Thread):
             return False
     
     def irrigation(self):
-        cookies = {'sysauth': '61fe7f9775453cc3b895be0da1202af8'}
+        cookies = {'sysauth': '87f2cda04e005dc61b8d5c8d81cadb5f'}
         lora_url = 'https://api.thingspeak.com/channels/970723/feeds.json?api_key=AU0TNWBNLRYXU1QL&results=5'
         lora_request = requests.get(lora_url).json()
         datas = lora_request["feeds"]
@@ -59,7 +61,7 @@ class sensor_worker(Thread):
                 # time을 분으로 환산
                 irr_time = int(((ga * area) / (1.6 * flow_rate)) * 3600)
 
-                str_irr_time = str(irr_time)
+                str_irr_time = "0"
                 
                 print("set time : ", irr_time)
 
@@ -68,14 +70,17 @@ class sensor_worker(Thread):
                 print(trigger_request, "request ON")
 
                 #insert usage of water to datebase
+                print("time : ", self.full_time)
                 post = {'water' : 100, 'dt' : self.full_time}
                 insert_id = collection.insert_one(post).inserted_id
-                print("Success Insert !! " , insert_id)
+                print("Irrigation data Inserted !! " , insert_id)
+                print("\n")
                 
         else:
             trigger_request = requests.get('http://192.168.2.241/arduino/irrigation/0', cookies=cookies)
             print("soil_moisture : ", soil_moisture)
             print(trigger_request, "request OFF")
+            print("\n")
     def run(self):
         while(True):
             self.irrigation()
@@ -92,9 +97,7 @@ class weather_worker(Thread):
             weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={},us&appid={}'
             city = 'Lafayette '
             weatehr_request = requests.get(weather_url.format(city, api_id)).json()
-            print(weather_url.format(city, api_id))
-            print(weatehr_request)
-            # check it is rain
+           # check it is rain
             if 'rain' not in weatehr_request:
                 print("not rain")
             else:
@@ -108,7 +111,7 @@ class weather_worker(Thread):
                 current_isodate = isodate.parse_datetime(full_time)
                 post = {'rainfall' : rainfall, 'dt' : current_isodate}
                 new_id = collection.insert_one(post).inserted_id
-                print("data was inserted!!  ", new_id)
+                print("Rain data inserted!!  ", new_id)
 
             time.sleep(3600)
 
